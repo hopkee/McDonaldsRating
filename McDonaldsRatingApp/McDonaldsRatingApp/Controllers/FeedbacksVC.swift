ea5e8cab601f0ad4541974caeff6cba7708b1480
Val <82764150+hopkee@users.noreply.github.com>
2022-02-22 02:40:48 +0300

    Update FeedbacksVC.swift

--- McDonaldsRatingApp/McDonaldsRatingApp/Controllers/FeedbacksVC.swift
+++ McDonaldsRatingApp/McDonaldsRatingApp/Controllers/FeedbacksVC.swift
@@ -31,14 +31,14 @@ extension FeedbacksVC: UITableViewDataSource {
         if let indexOfMeal = indexSelectedRow {
             return DataManager.shared.meals[indexOfMeal].feedbacks.count
         }
-        return 2
+        return 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ReviewCellVC", for: indexPath) as? ReviewCellVC
         if let indexOfRow = indexSelectedRow {
             cell?.nameLbl.text = DataManager.shared.meals[indexOfRow].feedbacks[indexPath.row].authorName
-            cell?.ratingBarLbl.text = DataManager.shared.meals[indexOfRow].ratingBar
+            cell?.ratingBarLbl.text = String(repeating: "⭐", count: Int(DataManager.shared.meals[indexOfRow].feedbacks[indexPath.row].mark.rounded(.up)))
             cell?.reviewText.text = DataManager.shared.meals[indexOfRow].feedbacks[indexPath.row].textReview
             cell?.likesLbl.isHidden = true
         }
